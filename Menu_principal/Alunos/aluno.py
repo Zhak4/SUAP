c38b5de84dabcc70433792878ccdd7e904bcc8b6
Eduardo Lima <108648404+yedulima@users.noreply.github.com>
2023-06-07 11:24:19 -0300

    Update aluno.py

--- Menu_principal/Alunos/aluno.py
+++ Menu_principal/Alunos/aluno.py
@@ -6,64 +6,92 @@ with open("Menu_principal/Alunos/alunos.json", "r") as file:
     alunos = json.load(file)
     
 def menu_aluno():
-    pergunta = int(input("Bem-vindo ao nosso modo aluno! selecione uma das opções abaixo: \n[1] Cadastrar aluno  \n[2] Visualizar alunos \n[3] Editar aluno \n[4] Apagar Alunos \nResposta -->: "))    
-    if pergunta == 1:
-        cadastrar_aluno()
-    elif pergunta == 2:
-        visualizar_aluno()
-    elif pergunta == 3:
-        editar_aluno()
-    elif pergunta == 4:
-        deletar_aluno()
+    
+    while True:
+
+        print(f"\n{'-== SISTEMA DE CONTROLE DE ALUNOS ==-': ^50}\n{'='*50}\n{'---==oOo==---': ^50}")
+        pergunta = input("Bem-vindo ao sistema de controle de alunos! \n[1] Cadastrar aluno  \n[2] Visualizar alunos \n[3] Editar aluno \n[4] Apagar Alunos \n\nOpção: ")
+
+        if pergunta == '1':
+            cadastrar_aluno()
+
+        elif pergunta == '2':
+            visualizar_aluno()
+
+        elif pergunta == '3':
+            editar_aluno()
+
+        elif pergunta == '4':
+            deletar_aluno()
+
+        else:
+            print("Opção inválida")
 
 def cadastrar_aluno():
-    nome = input("Digite o seu nome: ").title()
-    sobrenome = input("Digite o seu sobrenome: ").title()
+
+    print(f"\n{'-== CADASTRAR ALUNO ==- ': ^50}\n{'='*50}\n{'---==oOo==---': ^50}")
+    nome_aluno = input("Digite o seu nome: ").title()
+    sobrenome_aluno = input("Digite o seu sobrenome: ").title()
     
-    nome = nome + ' ' + sobrenome
-    if nome in alunos:
-        print("Este aluno já existe!")
-        
+    nome = nome_aluno + ' ' + sobrenome_aluno
+    
     CPF = input("Digite o seu CPF: ")
     
     if len(CPF) == 11:
-        CPF = f'{CPF[:3]}.{CPF[3:6]}.{CPF[6:9]}.{CPF[9:11]}'
-        print(CPF)
-    
+        CPF = f'{CPF[:3]}.{CPF[3:6]}.{CPF[6:9]}-{CPF[9:11]}'
+    else:
+        CPF_anterior = CPF
+        while CPF == CPF_anterior or len(CPF) != 11:
+            print("CPF inválido!")
+            CPF = input("Digite um CPF diferente: ")
     
     maior = 0
-    
+
     for aluno in alunos:
         if alunos[aluno]["Matricula"] > maior:
             maior = alunos[aluno]["Matricula"]
-
-    print(maior)
     
     alunos[nome] = {'Matricula': maior + 1, "Senha": CPF, "CPF": CPF}
 
-    continuar = int(input("Deseja continuar cadastrando alunos: \n[1] Sim \n[2] Não \nResp: "))
-    if continuar == 1:
-        cadastrar_aluno()
-    else:
-        menu_aluno()
-    
+    while True:
+        continuar = input("Deseja continuar cadastrando alunos: \n[1] Sim \n[2] Não \nResp: ")
+
+        if continuar == '1':
+            print(f"{f'---==oOo==---': ^50}")
+            cadastrar_aluno()
+
+        elif continuar == '2':
+            menu_aluno()
+        else:
+            print("Opção inválida!")
     
 def visualizar_aluno():
-    print("Estes são os alunos cadastrados: ")
-    
+
+    print(f"\n{'-== ALUNOS CADASTRADOS ==-': ^50}\n{'='*50}")
+
     for aluno in alunos:
-        print(aluno)
+        print(f"\n{f'---==oOo==---': ^50}\nAluno: {aluno}")
         for informacoes in alunos[aluno]:
-            print(informacoes, alunos[aluno][informacoes])
+            aluno_informacoes = alunos[aluno][informacoes]
+            if type(aluno_informacoes) == list:
+                print(f"{informacoes}:", end=' ')
+                for lista_informacoes in aluno_informacoes:
+                    if lista_informacoes != aluno_informacoes[-1]:
+                        print(lista_informacoes, end=', ')
+                    else:
+                        print(lista_informacoes)
+            else:
+                print(f"{informacoes}: {aluno_informacoes}")
+        print(f"{f'---==oOo==---': ^50}")
     
     menu_aluno()
     
-
 def editar_aluno():
-    buscar = input("Digite o nome do aluno que você deseja editar: ")
-    if buscar in alunos:
-        for informacoes in alunos[buscar]:
-            print(informacoes, alunos[buscar][informacoes])
+    buscar_aluno = input("Digite o nome do aluno que você deseja editar: ")
+    qtd_alunos = 0
+    if buscar_aluno in alunos:
+        for informacoes in alunos[buscar_aluno]:
+            print(informacoes, alunos[buscar_aluno][informacoes])
             
     edicao = int(input("O que você deseja alterar: \n[1] Nome e Sobrenome \n[2] Matrícula \n[3] Senha \nResp: "))
     if edicao == 1:
@@ -71,14 +99,14 @@ def editar_aluno():
         sobrenome = input("Digite o seu sobrenome: ")
         novo_nome = nome + ' ' + sobrenome
 
-        alunos[novo_nome] = alunos[buscar]
-        del alunos[buscar]
+        alunos[novo_nome] = alunos[buscar_aluno]
+        del alunos[buscar_aluno]
     elif edicao == 2:
         nova_matricula = int(input("Digite a sua matrícula: "))
-        alunos[buscar]['Matricula'] = nova_matricula
+        alunos[buscar_aluno]['Matricula'] = nova_matricula
     elif edicao == 3 :
         nova_senha = input("Digite a sua senha: ")
-        alunos[buscar]["Senha"] = nova_senha
+        alunos[buscar_aluno]["Senha"] = nova_senha
     
     
     repetir = int(input("Deseja continuar editando os alunos: \n[1] Sim \n[2] Não \nResp: "))
@@ -86,8 +114,7 @@ def editar_aluno():
         editar_aluno()
     else:
         menu_aluno()
-                
-            
+                           
 def deletar_aluno():
     encontrar = input("Digite o nome do aluno que você deseja apagar: ")
     if encontrar in alunos:
